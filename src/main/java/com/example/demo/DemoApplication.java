d430b8b728dd14d79745fdfca559b82186ba3be8
lishouxiang <1377212931@qq.com>
2022-04-19 21:43:28 +0800

    miaoshu

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//地方打算复读dsaadsfdssas
+
+		dskapkod
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
