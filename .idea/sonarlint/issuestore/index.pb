6b913f3de1f6f26edbbc9cbb94c645aab04ba01d
lishouxiang <1377212931@qq.com>
2022-05-05 01:03:03 +0800

    update .gitignore

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,19 @@
 
 a
 1src/main/java/com/example/demo/SplitLockDemo.java,f\5\f55c2ddb513d1a1f59de3dfb112322eb2db789e7
+c
+3src/main/java/com/example/demo/DemoApplication.java,7\1\7171df7c024808a26040ea210fc78aec82a83023
 e
 5src/main/java/com/example/demo/ReadWriteLockDemo.java,6\8\68561216f3be49b281cf59c0f72827fad89e5e16
 c
 3src/main/java/com/example/demo/ReenterLockDemo.java,9\d\9da2365498d9590cb3127abd862927be7c2c8d3f
-c
-3src/main/java/com/example/demo/DemoApplication.java,7\1\7171df7c024808a26040ea210fc78aec82a83023
-[
-+src/main/java/com/example/demo/CASDemo.java,3\e\3e9b66ca2c5ac3de6cdabaef68caaebf2269ed9c
 e
 5src/main/java/com/example/demo/ThreadLocalDemo01.java,d\f\dff148de7ccd631ac87702369659c2a82d71dde8
+[
++src/main/java/com/example/demo/CASDemo.java,3\e\3e9b66ca2c5ac3de6cdabaef68caaebf2269ed9c
+g
+7src/main/java/com/example/demo/AtomicReferenceDemo.java,9\0\90fc713322ec77a2eb214e9b2abcbdb84020f153
+[
++src/main/java/com/example/demo/ABADemo.java,c\e\ce6f18a96b43384ac815af945cb666829d8b9f64
+a
+1src/main/java/com/example/demo/ArrayListDemo.java,8\7\87524ab5df101c7842622ac99cc2ffa9d2bc9593
--- src/main/java/com/example/demo/ArrayListDemo.java
+++ src/main/java/com/example/demo/ArrayListDemo.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -14,6 +15,76 @@ import java.util.concurrent.CopyOnWriteArraySet;
  */
 public class ArrayListDemo {
     public static void main(String[] args) {
+          //listNotSafe();
+         //setNotSafe();
+        //MapNotSafe();
+
+    }
+
+    public static void MapNotSafe() {
+        Map<String,Object> map = new ConcurrentHashMap<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+
+        }
+
+        /**
+         * 1.异常故障
+         * java.util.ConcurrentModificationException
+         *
+         * 2.解决方案
+         * 2.1 Collections.synchronizedMap(new HashMap<>());
+         * 2.2 new ConcurrentHashMap<>()
+         *
+         */}
+
+    public static void setNotSafe() {
+        Set<String> set = new CopyOnWriteArraySet<>();
+        //Collections.synchronizedSet(new HashSet<>());
+        //new HashSet<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
+            }, String.valueOf(i)).start();
+
+        }
+        /**
+         * 1.报错异常，故障现象
+         * java.util.ConcurrentModificationException
+         *
+         * 2.解决方案
+         * Collections.synchronizedSet(new HashSet<>());
+         *
+         * 3.底层代码
+         *  private final CopyOnWriteArrayList<E> al;
+         *  public CopyOnWriteArraySet() {
+         *  al = new CopyOnWriteArrayList<E>();
+         *  }
+         *  可以看出 new CopyOnWriteArraySet<>() 底层
+         *  也是使用了 new CopyOnWriteArrayList<E>();
+         *
+         *
+         */
+        new HashSet<>().add("c");
+        /**
+         * HashSet<>()的底层源码是
+         *  public HashSet() {
+         *         map = new HashMap<>();
+         *     }
+         * 那为什么set的add天添加方法是一个元素而不是两个元素？
+         * 从源码可以看出 map的value是一个常量值 Object
+         * private static final Object PRESENT = new Object();
+         *  public boolean add(E e) {
+         *         return map.put(e, PRESENT)==null;
+         *     }
+         *
+         */}
+
+    private static void listNotSafe() {
         List list = new CopyOnWriteArrayList();//写时复制，读写分离的思想
         Set set = new CopyOnWriteArraySet();
 //        list.add("a");
@@ -43,30 +114,30 @@ public class ArrayListDemo {
          * 4.优化建议
          */
 
-    }
-    /**
-     * copyOnWrite容器即写时复制容器，往一个容器添加元素的时候，不会直接往Object[] 去添加，而是先将Object[]容器进行copy
-     * 复制出一个新的容器 Object[] elements,然后新的容器里Object[] elements 添加元素，添加完元素之后，再将原容器的指向，指向新的容器
-     *  setArray(newElements) ，这样做的好处时并发对容器copyOnWrite读的时候不需要进行加锁，因为当前容器不会添加任何元素
-     *  所以copyOnWrite容器也是一种读写分离的思想，都和写不同容器
-     *
-     *
-     *   public boolean add(E e) {
-     *         final ReentrantLock lock = this.lock;
-     *         lock.lock();
-     *         try {
-     *             Object[] elements = getArray();
-     *             int len = elements.length;
-     *             Object[] newElements = Arrays.copyOf(elements, len + 1);
-     *             newElements[len] = e;
-     *             setArray(newElements);
-     *             return true;
-     *         } finally {
-     *             lock.unlock();
-     *         }
-     *     }
-     *
-     *
-     */
+        /**
+         * copyOnWrite容器即写时复制容器，往一个容器添加元素的时候，不会直接往Object[] 去添加，而是先将Object[]容器进行copy
+         * 复制出一个新的容器 Object[] elements,然后新的容器里Object[] elements 添加元素，添加完元素之后，再将原容器的指向，指向新的容器
+         *  setArray(newElements) ，这样做的好处时并发对容器copyOnWrite读的时候不需要进行加锁，因为当前容器不会添加任何元素
+         *  所以copyOnWrite容器也是一种读写分离的思想，都和写不同容器
+         *
+         *
+         *   public boolean add(E e) {
+         *         final ReentrantLock lock = this.lock;
+         *         lock.lock();
+         *         try {
+         *             Object[] elements = getArray();
+         *             int len = elements.length;
+         *             Object[] newElements = Arrays.copyOf(elements, len + 1);
+         *             newElements[len] = e;
+         *             setArray(newElements);
+         *             return true;
+         *         } finally {
+         *             lock.unlock();
+         *         }
+         *     }
+         *
+         *
+         */}
+
 }
 
