1486c096a82dc92666e39be4f7e1a9cf9906d3fa
lishouxiang <1377212931@qq.com>
2022-05-08 17:21:54 +0800

    CyclicBarrier 与countDownLatch相反，只有集齐才可以开始工作

--- src/main/java/com/concurrent/CutDownLatchDemo.java
+++ src/main/java/com/concurrent/CutDownLatchDemo.java
@@ -1,6 +1,7 @@
 package com.concurrent;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 /**
  * @author lishouxiang
@@ -23,15 +24,4 @@ public class CutDownLatchDemo {
         System.out.println(Thread.currentThread().getName()+"\t 秦灭六国，一统华夏");
     }
   
-    public static void closeDoor() throws InterruptedException {
-        CountDownLatch cutDown = new CountDownLatch(6);
-        for (int i = 1; i <= 6; i++) {
-            new Thread(() -> {
-                System.out.println(Thread.currentThread().getName()+"\t 上完自习，离开教室走人");
-                cutDown.countDown();
-            }, String.valueOf(i)).start();
-        }
-         cutDown.await();
-        System.out.println(Thread.currentThread().getName()+"\t 88班长锁门，走人");
-    }
 }
---
+++ src/main/java/com/concurrent/CyclicBarrierDemo.java
@@ -0,0 +1,29 @@
+package com.concurrent;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ *
+ */
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("集齐龙珠召唤神龙");
+        });
+
+        for (int i = 1; i <= 7; i++) {
+            final int temp = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 集齐到+" + temp + "颗龙珠");
+                try {
+                    cyclicBarrier.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
